9e1461b075454776608b1dc25ff40d23fc99a8f7
KumaStark <elastic48@msn.com>
2020-09-11 21:08:45 +0800

    Vue - Day 3 - Homework

---
+++ Homeworks/20200909-Vue2.0-03/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+      <Button :[switchName]="disabled" v-slot:btnTitle v-on:click="hello(buttonName,$event)">{{switchName}}:</Button>
+  </div>
+</template>
+
+<script>
+import Button from "./view/Button";
+export default {
+  name: "App",
+  components: {
+    Button,
+  },
+  data(){return{
+    disabled:false,
+    switchName:"disabled",
+    buttonName:"TestBtn",
+  }},
+  methods: {
+    hello(m,e) {
+      console.log(`Get Emit from : ${m}`);
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  margin-top: 60px;
+}
+</style>
---
+++ Homeworks/20200909-Vue2.0-03/view/Button.vue
@@ -0,0 +1,37 @@
+<template>
+    <button v-if="disabledSwicth==true" disabled>
+      <slot name="btnTitle">默认禁用</slot>{{disabledSwicth}}
+    </button>
+    <button v-else v-on:click="btnClicked">
+      <slot name="btnTitle">默认启用</slot>{{disabledSwicth}}
+    </button>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data(){
+    return {
+      disabledSwicth:this.disabled,
+    }
+  },
+  methods: {
+    btnClicked() {
+      console.log("Button Clicked !");
+      this.$emit("click");
+      this.disabledSwicth = !this.disabledSwicth;
+      // setTimeout(() => {
+      //   this.disabledSwicth = !this.disabledSwicth;
+      // }, 3000);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
